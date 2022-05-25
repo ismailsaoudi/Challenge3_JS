57f71ab29c2c6ebb89d50fafe6b04e866ff19322
ismail saoudi <ismail.saoudi@icloud.com>
2022-05-25 12:00:46 -0400

    preventdefault added

--- assets/script.js
+++ assets/script.js
@@ -7,20 +7,27 @@ var charactersAmountNumber = document.getElementById ( 'CharactersAmountNumber' 
 CharactersAmountNumber.addEventListener('input', syncharacterAmount)
 CharactersAmountRange.addEventListener('input', syncharacterAmount)
 
+// Get references to the #generate element
+var passwordText = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener ('submit', e => {
+  e.preventdefault()
+})
+
 function syncharacterAmount(e) {
 var value = e.target.value
 CharactersAmountNumber.value = value
 CharactersAmountRange.value = value
 
 }
-console.log(syncharacterAmount)
+
 // Assignment code here
-var passwordText = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
 function writePassword() {
