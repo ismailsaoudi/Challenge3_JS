4b845f3775bc0df6779470740145d87094aeaa9a
ismail saoudi <ismail.saoudi@icloud.com>
2022-05-27 09:52:58 -0400

    changing Form style

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,44 @@
-// when the user press generate password 
-// number of characters share value betwwn slider and number box
-
-var charactersAmountRange = document.getElementById ( 'CharactersAmountRange' )
-var charactersAmountNumber = document.getElementById ( 'CharactersAmountNumber' )
-// var passwordText = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// var generateBtn = document.getElementById("generate");
+var charactersAmountRange = document.getElementById ("CharactersAmountRange")
+var charactersAmountNumber = document.getElementById ("CharactersAmountNumber")
 var includeAmountNumbersEl = document.getElementById("includeUppercase")
 var includeSymbolsEl = document.getElementById("includeSymbols")
 var includeUppercaseEl = document.getElementById("includeUppercase")
-
 var UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 var LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 var SYMBOLS_CHAR_CODES = arrayFromLowToHigh(33, 47)
 var NUMBERS_CHAR_CODES = arrayFromLowToHigh(48, 57)
-
-
-// var password = generatePassword( characterAmount, includeNumbers, includeSymbols, includeUppercase)
 var labelimputs = document.getElementById("passwordGeneratorForm")
 
 CharactersAmountNumber.addEventListener('input', syncharacterAmount)
 CharactersAmountRange.addEventListener('input', syncharacterAmount)
 
+function syncharacterAmount(e) {
+  var value = e.target.value
+  CharactersAmountNumber.value = value
+  CharactersAmountRange.value = value
+  }
+
+
 labelimputs.addEventListener ('submit', e=> {
-  e.preventDefault()
+e.preventDefault()
 var characterAmount = charactersAmountNumber.value
 var includeUppercase = includeUppercase.checked
 var includeSymbols = includeSymbolsEl.checked
 var includeNumbers = includeAmountNumbersEl.checked
-var password = generatePassword(charactersAmount, includeUppercase, includeSymbols, includeNumbers )
+var password = generatePassword(charactersAmount, includeUppercase, includeSymbols, includeNumbers) 
 })
 
-function generatePassword(characterAmount, includeNumbers, includeSymbols, includeUppercase){
-let charCodes = LOWERCASE_CHAR_CODES
-if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-if (includeNumbers) charCodes = charCodes.concat(NUMBERS_CHAR_CODES)
-if (includeSymbols) charCodes = charCodes.concat(SYMBOLS_CHAR_CODES)
-var passwordcharacters = []
-for (let i=0; i< characterAmount, i++)
-passwordcharacters.push(character)
- String.fromCharCode() 
-}
-
+// function generatePassword(characterAmount, includeNumbers, includeSymbols, includeUppercase){
+// var charCodes = LOWERCASE_CHAR_CODES
+// if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+// if (includeNumbers) charCodes = charCodes.concat(NUMBERS_CHAR_CODES)
+// if (includeSymbols) charCodes = charCodes.concat(SYMBOLS_CHAR_CODES)
+// var passwordcharacters = []
+// for (let i=0; i < characterAmount, i++){
+// passwordcharacters.push(character)
+//  String.fromCharCode() 
+// }
+// }
 
 function arrayFromLowToHigh(low, high){
 var array = []
@@ -49,18 +47,6 @@ array.push(i)
   }
   return array
 }
-// Get references to the #generate element
-
-
-
-function syncharacterAmount(e) {
-  var value = e.target.value
-  CharactersAmountNumber.value = value
-  CharactersAmountRange.value = value
-  
-  }
-
-  // function generatePassword(characterAmount, includeNumbers, includeSymbols, includeUppercase)
 
 
 
@@ -69,24 +55,7 @@ function syncharacterAmount(e) {
 
 
 
-// Assignment code here
 
-
-
-
-
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
 
 
 
